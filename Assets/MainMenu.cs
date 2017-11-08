922cf56e811b4dab090b0612b30cb7de3e94310e
kubastyk1 <kubastyk@wp.pl>
2017-11-08 15:26:50 +0100

    Added main menu to game.

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public void OnStartButton()
+    {
+        SceneManager.LoadScene("FirstScene");
+    }
+
+    public void OnRestartButton()
+    {
+        SceneManager.LoadScene("FirstScene");
+    }
+
+    public void OnExitButton()
+    {
+        Application.Quit();
+    }
+}
