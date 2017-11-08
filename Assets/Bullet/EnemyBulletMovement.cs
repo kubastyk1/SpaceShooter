3f2927816f14de0b61dc3452df8008edd4903916
kubastyk1 <kubastyk@wp.pl>
2017-11-08 16:12:07 +0100

    Added enemy spacecraft with shooting.

---
+++ Assets/Bullet/EnemyBulletMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBulletMovement : MonoBehaviour {
+
+    float maxSpeed = 5f;
+
+    void Update()
+    {
+        Vector3 position = transform.position;
+        position.y -= maxSpeed * Time.deltaTime;
+        transform.position = position;
+    }
+}
---
+++ Assets/Spacecraft/EnemySpacecraftMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpacecraftMovement : MonoBehaviour {
+
+    float maxSpeed = 2f;
+
+    void Update()
+    {
+        Vector3 spacecraftPosition = transform.position;
+        spacecraftPosition.y -= maxSpeed * Time.deltaTime;
+
+        transform.position = spacecraftPosition;
+    }
+}
