2ed3ad3838005b5fbe98c2a87b058bc26abe9866
kubastyk1 <kubastyk@wp.pl>
2017-11-08 16:21:40 +0100

    Added enemy spacecraft random movement.

--- Assets/Spacecraft/EnemySpacecraftMovement.cs
+++ Assets/Spacecraft/EnemySpacecraftMovement.cs
@@ -4,12 +4,41 @@ using UnityEngine;
 
 public class EnemySpacecraftMovement : MonoBehaviour {
 
-    float maxSpeed = 2f;
+    public float maxSpeed = 2f;
+    int turnDelay = 0;
+    float randomXPosition = 0;
+    float startXPosition = 0;
+
+    void Start()
+    {
+        randomXPosition = Random.Range(0, 0.1f);
+        randomXPosition = Random.Range(0, 0.05f);
+        startXPosition = transform.position.x;
+    }
 
     void Update()
     {
         Vector3 spacecraftPosition = transform.position;
         spacecraftPosition.y -= maxSpeed * Time.deltaTime;
+
+        if (turnDelay < 40)
+        {
+            if (startXPosition < 0)
+            {
+                spacecraftPosition.x += randomXPosition;
+            }
+            else
+            {
+                spacecraftPosition.x -= randomXPosition;
+            }
+            turnDelay++;
+        }
+        else
+        {
+            randomXPosition = Random.Range(0, 0.1f);
+            startXPosition = transform.position.x;
+            turnDelay = 0;
+        }
 
         transform.position = spacecraftPosition;
     }
